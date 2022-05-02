20054a3c8ea816bab5cba79e5c7d898e0038c63a
Ralph Cajipe <ralphcajipe@gmail.com>
2022-05-02 22:25:56 +0800

    Resume version #01

---
+++ resume/resume.php
@@ -0,0 +1,149 @@
+<html lang="en">
+
+<head>
+    <title>Resume | Ralph Cajipe</title>
+<body>
+
+<center>
+<?php
+// Name 
+echo '<h1>Ralph Cajipe</h1>';
+echo 'Manila, PH | <a href="https://www.linkedin.com/in/ralphcajipe/" target="_blank"> LinkedIn</a> |
+<a href="https://github.com/ralphcajipe/" target="_blank"> GitHub</a> | 
++63 916 666 5832 | ralphcajipe@gmail.com<br>';
+?>
+</center>
+
+
+<?php
+// Define string variables
+$line = "&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; ___________________________________________________________________________________________________________________________";
+
+// EDUCATION variables
+//$education = str_replace(["\n", "\r",], "", $education);
+$education = "&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; EDUCATION";
+
+// EDUCATION - FEU Tech
+$feu_tech = "&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; FEU Institute of Technology 
+&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; 
+&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp;
+&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; Sampaloc, Manila";
+
+$bscsse = "&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; BS in Computer Science, Software Engineering
+&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp;  
+&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; 
+&emsp; &emsp; Graduation Date: 2023";
+
+echo "<b>$education<br>
+$line <br>
+</b>";
+echo "<b>$feu_tech</b> <br>
+<i>$bscsse</i> <br>
+";
+
+// EDUCATION - Wesleyan University - Philippines
+$wup = "&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; Wesleyan University - Philippines
+&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; 
+&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp;
+&emsp; &emsp; &emsp; &emsp; &emsp;Cabanatuan, Nueva Ecija";
+
+$bscs = "&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; BS in Computer Science
+&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp;  
+&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; 
+&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp;
+ SY 2018 - 2019";
+
+
+echo "<br>";
+echo "<b>$wup</b> <br>
+<i>$bscs</i> <br>
+";
+
+
+// WORK EXPERIENCE variables
+echo "<br>";
+$work = "&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; WORK EXPERIENCE";
+
+$freelance = "&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; Freelance
+&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; 
+&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp;
+&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; 
+&emsp; &emsp; &emsp; &emsp;  &emsp; Remote Work";
+
+$python = "&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; Python Software Developer
+&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp;  
+&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; 
+&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; April 2022 - Present";
+
+$responsibility = "&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; •&emsp;
+Writes, tests, and debugs efficient software written in Python programming language. <br>
+&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; •&emsp;
+Applies mathematical foundations, algorithmic principles, and computer science theory in the 
+modeling and design of computer based systems in a way <br> 
+&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp;
+ that demonstrate comprehension of the tradeoffs involved in design choices and applicable standards. 
+";
+
+echo "<b>$work<br>
+$line <br>
+</b>";
+echo "<b>$freelance</b> <br>
+<i>$python</i> <br>
+$responsibility <br>";
+
+
+// COURSE EXPERIENCE variables
+echo "<br>";
+$course = "&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; COURSE EXPERIENCE";
+
+$cs50 = "&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; Harvard's CS50 (CS50x,Python)
+&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; 
+&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp;
+&emsp; &emsp; &emsp; &emsp; &emsp; &emsp;Online OpenCourseWare";
+
+$student = "&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; Student
+&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp;  
+&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; 
+&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; 
+&emsp; &emsp; &emsp; &emsp; August 2018 - Present";
+
+$xp = "&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; •&emsp;Programmed procedural and 
+functional programs in C for good understanding of computer memory and algorithms. <br>
+&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; •&emsp;Wrote reusable, testable, and efficient Python 3 code. <br>
+&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; •&emsp;Solved problems using the Structured programming approach, 
+aimed at improving the clarity, quality, and development time for Python. <br>
+&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; •&emsp;Utlized GitHub Codespaces (VS Code) as IDE workspace. <br>
+&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; •&emsp;Utilized Bash (Bourne Again Shell) to accomplish tasks and to execute commands read from the standard input or from a file. <br>
+&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; •&emsp;Experienced using Git and GitHub for version control. <br>
+";
+
+echo "<b>$course<br>
+$line <br>
+</b>";
+echo "<b>$cs50</b> <br>
+<i>$student</i> <br>
+$xp <br>";
+
+
+// SKILLS & INTERESTS variables
+echo "<br>";
+$skills_interests = "&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; SKILLS & INTERESTS";
+
+$skills = "&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; <b>Skills:</b> Programming (Python) |
+Algorithms | PyCharm | VS Code | Linux Bash | scikit-learn | TensorFlow | keras <br>";
+
+$interests = "&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; <b>Interests:</b> 
+Artificial Intelligence | Machine Learning | Computer Vision | 
+Natural Language Processing | Speech Recognition | Anime/Manga <br>";
+
+
+
+echo "<b>$skills_interests<br>
+$line <br>
+</b>";
+echo "$skills <br>
+$interests <br>";
+
+?>
+</body>
+</html>
