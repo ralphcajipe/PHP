bc09e87ac56632f3047230f7f22d8e68ede8c2da
Ralph Cajipe <ralphcajipe@gmail.com>
2022-06-29 15:06:48 +0800

    Add sa3

---
+++ summative/sa3/login.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+    <title>Login</title>
+</head>
+
+<!--
+Create a login module just like what is shown below.  
+Using setcookie() function, create cookies for the username and password.
+It must be saved in your web form if the remember me checkbox is checked only. 
+
+Username
+Password
+Remember me checkbox (if checked, create cookies for username and password)
+Submit button
+
+-->
+
+
+</html>
